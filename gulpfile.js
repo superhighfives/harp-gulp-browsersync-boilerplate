9250801ec6af170b63c552125c62f91e2bf473cb
Charlie Gleason <hi@superhighfives.com>
2014-08-19 17:37:14 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+var harp        = require('harp');
+
+/**
+ * Serve the Harp Site from the src directory
+ */
+gulp.task('serve', function () {
+  harp.server(__dirname, {
+    port: 9000
+  }, function () {
+    browserSync({
+      proxy: "localhost:9000",
+      open: false,
+      /* Hide the notification. It gets annoying */
+      notify: {
+        styles: ['opacity: 0', 'position: absolute']
+      }
+    });
+    /**
+     * Watch for scss changes, tell BrowserSync to refresh main.css
+     */
+    gulp.watch("*.sass", function () {
+      reload("main.css", {stream: true});
+    });
+    /**
+     * Watch for all other changes, reload the whole page
+     */
+    gulp.watch(["*.ejs"], function () {
+      reload();
+    });
+  })
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the harp site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['serve']);
